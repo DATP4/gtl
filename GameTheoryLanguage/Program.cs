a84f8ce5a1effb3e7f8b1ea456fdcd05930cbf35
Martin <enevoldsen2oo@gmail.com>
2024-03-17 19:06:35 +0100

    initial grammar

--- GameTheoryLanguage/Program.cs
+++ GameTheoryLanguage/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+
+static void MyParseMethod()
+{
+    // Read input from a file
+    string filePath = "GameTheoryLanguage/parser/input.gtl"; // Update the file path accordingly
+    string input = File.ReadAllText(filePath);
+
+    // Create a stream from the input string
+    ICharStream stream = CharStreams.fromString(input);
+
+    // Create a lexer
+    ITokenSource lexer = new ExprLexer(stream);
+
+    // Create a token stream
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    // Print the different tokens
+    tokens.Fill();
+    foreach (var token in tokens.GetTokens())
+    {
+        Console.WriteLine("Token: " + token);
+    }
+
+    // Reset the token stream position to the beginning
+    tokens.Reset();
+
+    // Create a parser
+    ExprParser parser = new ExprParser(tokens);
+}
+
+MyParseMethod();
