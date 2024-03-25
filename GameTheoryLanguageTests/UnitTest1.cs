5915b0cf2e919f15ba468d1e645463877bda0ad9
Guldbaek2 <mguldb22@student.aau.dk>
2024-03-25 11:02:16 +0100

    create test

--- GameTheoryLanguageTests/UnitTest1.cs
+++ GameTheoryLanguageTests/UnitTest1.cs
@@ -2,7 +2,33 @@ namespace GameTheoryLanguageTests;
 
 [TestClass]
 public class UnitTest1
+
 {
+    public string parse (string path) {
+    // Read input from a file
+    // Update the file path accordingly
+    string input = File.ReadAllText(path);
+
+    // Create a stream from the input string
+    ICharStream stream = CharStreams.fromString(input);
+
+    // Create a lexer
+    ITokenSource lexer = new GtlLexer(stream);
+
+    // Create a token stream
+    // Change this to ITokenStream
+    CommonTokenStream tokens = new(lexer);
+
+    // Create a parser
+    GtlParser parser = new(tokens);
+    IParseTree tree = parser.program();
+
+    return tree.ToStringTree(parser);
+    }
+
     [TestMethod]
-    public void TestMethod1() { }
+    public void TestMethod1() {
+        string output = parse("./IfElseTest.gtl");
+        
+    }
 }
