ea7e54ed4543cc4b190c7bd3d161fe4137207cd0
Guldbaek2 <mguldb22@student.aau.dk>
2024-05-01 11:57:24 +0200

    test

--- GameTheoryLanguageTests/EndToEndTests.cs
+++ GameTheoryLanguageTests/EndToEndTests.cs
@@ -42,32 +42,54 @@ public class EndToEndTests
     private void DeclarationTest()
     {
         Createtest("int x = 5;", "assert_eq!(x, 5)", "declaration");
-        Createtest("real x = 5.0;", "assert_eq!(x, 5.0);", "declaration");
-        Createtest("bool x = TRUE;", "assert_eq!(x, true);", "declaration");
+        Createtest("real x = 5.0;", "assert_eq!(x, 5.0)", "declaration");
+        Createtest("bool x = TRUE;", "assert_eq!(x, true)", "declaration");
     }
     private void BinaryExpressionTest()
     {
-
+        Createtest("int test = 1 + 1 * 7;", "assert_eq!(test, 8)", "binary_expression");
+        Createtest("real test = 1.5 + 1.6 / 10.0;", "assert_eq!(test, 1.66)", "binary_expression");
+        Createtest("int test = -5 + ( 4 + 3 * ( 4 MOD 5 ) / 1 + 5 ) - 3;", "assert_eq!(test, 13)", "binary_expression");
+        Createtest("int test = 5 MOD 3;", "assert_eq!(test, 2)", "binary_expression");
     }
     private void BooleanExpressionTest()
     {
-
+        Createtest("bool test1 = TRUE && FALSE;", "assert_eq!(test1, false)", "boolean_expression");
+        Createtest("bool test1 = 1 > 2;", "assert_eq!(test1, false)", "boolean_expression");
+        Createtest("bool test1 = 1 != 0;", "assert_eq!(test1, true)", "boolean_expression");
+        Createtest("bool test1 = 1 <= 2;", "assert_eq!(test1, true)", "boolean_expression");
+        Createtest("bool test1 = (TRUE && TRUE) || ((TRUE && TRUE) == TRUE);", "assert_eq!(test1, true)", "boolean_expression");
     }
     private void LogicalNotTest()
     {
-
+        Createtest("bool test1 = !(TRUE && FALSE);", "assert_eq!(test1, true)", "logical_not");
+        Createtest("bool test1 = !(1 > 2);", "assert_eq!(test1, true)", "logical_not");
+        Createtest("bool test1 = !(1 != 0);", "assert_eq!(test1, false)", "logical_not");
+        Createtest("bool test1 = !(1 <= 2);", "assert_eq!(test1, false)", "logical_not");
+        Createtest("bool test1 = !(!(TRUE && TRUE) || ((TRUE && TRUE) == TRUE));", "assert_eq!(test1, false)", "logical_not");
     }
     private void UnaryExpressionTest()
     {
-
+        Createtest("int x = -5;", "assert_eq!(x, -5)", "unary_expression");
+        Createtest("real x = -5.0;", "assert_eq!(x, -5.0)", "unary_expression");
+        Createtest("int x = -5 - -5;", "assert_eq!(x, 0)", "unary_expression");
+        Createtest("int x = -5 + -5;", "assert_eq!(x, -10)", "unary_expression");
+        Createtest("int x = -5; int y = -x;", "assert_eq!(y, 5)", "unary_expression");
     }
     private void IfElseTest()
     {
-
+        Createtest("int y = if (TRUE) then {int x = 4;} else {int x = 5;};", "assert_eq!(y, 4)", "if_else");
+        Createtest("int y = if (FALSE) then {int x = 4;} else {int x = 5;};", "assert_eq!(y, 5)", "if_else");
+        Createtest("int y = if (TRUE) then {int x = 4;} else if (TRUE) then {int x = 5;} else {int x = 6;};", "assert_eq!(y, 4)", "if_else");
+        Createtest("int y = if (FALSE) then {int x = 4;} else if (TRUE) then {int x = 5;} else {int x = 6;};", "assert_eq!(y, 5)", "if_else");
+        Createtest("int y = if (FALSE) then {int x = 4;} else if (FALSE) then {int x = 5;} else {int x = 6;};", "assert_eq!(y, 6)", "if_else");
     }
     private void FunctionTest()
     {
-
+        Createtest("int_function : (int x) -> int {int y = x + 10 * 5; y - 5;} int x = int_function(5);", "assert_eq!(x, 50)", "function");
+        Createtest("int a = 10; int_function1 : (int x) -> int {int y = x + a * 5; y - 5;} int x = int_function1(5);", "assert_eq!(x, 50)", "function");
+        Createtest("int_function : (int x) -> int {int_function2 : (int x) -> int {x + 5;} int y = int_function2(x);} int x = int_function(5);", "assert_eq!(x, 10)", "function");
+        Createtest("int a = 10 + 5; int b = 5 + 13; int c = a * b; int_function : (int x) -> int {c + x;} int x = int_function(10);", "assert_eq!(x, 280)", "function");
     }
     private void ActionDeclarationTest()
     {
@@ -121,7 +143,7 @@ public class EndToEndTests
             {
                 Testcounter = 1;
             }
-            Outputfile.Add($"#[test]\nfn {Testtype}_test{Testcounter}()" + "{\n");
+            Outputfile.Add($"#[test]\nfn {Testtype}_test_{Testcounter}()" + "{\n");
             Testcounter += 1;
             Previoustesttype = Testtype;
             // Program consists of statements only, so we iterate them
