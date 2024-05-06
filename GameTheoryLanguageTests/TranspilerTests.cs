2880e53331841f118a31d155915aff29a30b43d4
MikkelBaekgaardJoergensen <SE38IZ@student.aau.dk>
2024-05-06 13:04:22 +0200

    Implemented some integration tests

--- GameTheoryLanguageTests/TranspilerTests.cs
+++ GameTheoryLanguageTests/TranspilerTests.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
 namespace GameTheoryLanguageTests;
@@ -10,16 +12,20 @@ public class TranspilerTests
     public void AssignmentTest()
     {
         string input1 = "int x = 3;";
-        string input2 = "bool x = TRUE;";
-        string input3 = "real x = 3.0;";
+        string input2 = "int x = 3.0;";
+        string input3 = "bool x = TRUE;";
+        string input4 = "real x = 3.0;";
 
         string output1 = "let x = 3;";
-        string output2 = "let x = true;";
-        string output3 = "let x = 3.0;";
+        string output2 = "let x = 3.0;";
+        string output3 = "let x = true;";
+        string output4 = "let x = 3.0;";
 
-        AssertTrue(input1, output1);
+        AssertIntegrationTrue(input1, output1);
+        AssertIntegrationFalse(input2);
         AssertTrue(input2, output2);
         AssertTrue(input3, output3);
+        AssertTrue(input4, output4);
     }
     [TestMethod]
     public void BooleanExpressionTest()
@@ -28,7 +34,8 @@ public class TranspilerTests
         string input2 = "bool test = 1 > 2;";
         string input3 = "bool test = 1 != 0;";
         string input4 = "bool test = 1 <= 2;";
-        string input5 = "bool test = (TRUE && TRUE) || ((TRUE && TRUE) == TRUE);";
+        string input5 = "real test = (TRUE && TRUE) || ((TRUE && TRUE) == TRUE);";
+        string input6 = "bool test = (TRUE && TRUE) || ((TRUE && TRUE) == TRUE);";
 
         string output1 = "let test = true && false;";
         string output2 = "let test = 1 > 2;";
@@ -41,6 +48,8 @@ public class TranspilerTests
         AssertTrue(input3, output3);
         AssertTrue(input4, output4);
         AssertTrue(input5, output5);
+        AssertIntegrationFalse(input5);
+        AssertIntegrationTrue(input6, output5);
     }
     [TestMethod]
     public void BinaryExpressionTest()
@@ -49,16 +58,21 @@ public class TranspilerTests
         string input2 = "real test = 1.5 + 1.6 / 10.0;";
         string input3 = "int test = (-5 + ( 4 + 3 * ( 4 MOD 5 ) / 1 + 5 ) - 3);";
         string input4 = "5 MOD 3;";
+        string input5 = "int test = 1 + 1 * TRUE;";
 
         string output1 = "let test = 1 + 1 * 7;";
         string output2 = "let test = 1.5 + 1.6 / 10.0;";
         string output3 = "let test = (-5 + (4 + 3 * (4 % 5) / 1 + 5) - 3);";
         string output4 = "5 % 3;";
+        string output5 = "let test = 1 + 1 * true;";
 
         AssertTrue(input1, output1);
+        AssertIntegrationTrue(input2, output2);
         AssertTrue(input2, output2);
         AssertTrue(input3, output3);
         AssertTrue(input4, output4);
+        AssertTrue(input5, output5);
+        AssertIntegrationFalse(input5);
     }
     [TestMethod]
     public void IfElseTest()
@@ -67,16 +81,20 @@ public class TranspilerTests
         string input2 = "if (TRUE) then {int x = 4; x;} else if (FALSE) then {int y = 5; y;} else {int z = 6; z;};";
         string input3 = "if (TRUE) then {int x = 4;} else if (FALSE) then {int y = 4;} else {int z = 4;};";
         string input4 = "int x = if (TRUE) then {int x = 4;} else if (FALSE) then {int y = 3;} else {int z = 2;};";
+        string input5 = "real x = if (TRUE) then {int x = 4;} else if (FALSE) then {int y = 3;} else {int z = 2;};";
 
         string output1 = "if true {let x = 4;x} else {let y = 5;y};";
         string output2 = "if true {let x = 4;x} else if false {let y = 5;y} else {let z = 6;z};";
         string output3 = "if true {let x = 4;x} else if false {let y = 4;y} else {let z = 4;z};";
-        string output4 = "let x = if true {let x = 4;x} else if false {let y = 3;y} else {let z = 2;z};";
+        string output4_5 = "let x = if true {let x = 4;x} else if false {let y = 3;y} else {let z = 2;z};";
 
         AssertTrue(input1, output1); // Regular if else statement with assignment to expression return value
         AssertTrue(input2, output2); // If else if else with return value
         AssertTrue(input3, output3); // If else if else with with assignment to expression return value
-        AssertTrue(input4, output4); // Variable assignment to if else if else statement
+        AssertTrue(input4, output4_5); // Variable assignment to if else if else statement
+        AssertIntegrationTrue(input4, output4_5); // Same as the above, but passess the type checker too
+        AssertIntegrationFalse(input5); // If else statement return int to real
+
     }
 
     [TestMethod]
@@ -86,18 +104,22 @@ public class TranspilerTests
         string input2 = "intFunction : (int x) -> int {if (TRUE) then {5;} else {6;};}";
         string input3 = "intFunction : (int x) -> int {if (TRUE) then {x;intFunctionNested : (int z) -> int {1;}} else {6;};}";
         string input4 = "int x = 5; intFunction : (int z) -> int {int q = 3; z; intFunctionNested : (int y) -> int {x + 1;}}";
-
+        string input5 = "intFunction : (int x) -> int {if (TRUE) then {5.1;} else {6;};}";
 
         string output1 = "fn intFunction(x: &i32) -> i32 {let y = *x + 10 * 5;*x - 5}";
         string output2 = "fn intFunction(x: &i32) -> i32 {if true {5} else {6}}";
         string output3 = "fn intFunction(x: &i32) -> i32 {if true {fn intFunctionNested(z: &i32) -> i32 {1}*x} else {6}}";
         string output4 = "let x = 5;fn intFunction(z: &i32) -> i32 {let x = 5;let q = 3;fn intFunctionNested(y: &i32) -> i32 {let x = 5;let q = 3;x + 1}*z}";
+        string output5 = "fn intFunction(x: &i32) -> i32 {if true {5.1} else {6}}";
 
         // Also tests conversion to call by reference
         AssertTrue(input1, output1); // Regular function test
         AssertTrue(input2, output2); // If else in function
         AssertTrue(input3, output3); // Nested function
         AssertTrue(input4, output4); // Nested function with outer scope variables inclusion
+        AssertTrue(input5, output5);
+        AssertIntegrationFalse(input5);
+        AssertIntegrationTrue(input4, output4);
     }
 
     [TestMethod]
@@ -108,12 +130,16 @@ public class TranspilerTests
         string input3 = "intFunction : (int x) -> int {intFunctionNested : (int z) -> int {z + 1;} intFunctionNested(x);} intFunction(5);";
         string input4 = "intFunction : (int x) -> int {int y = x + 10 * 5;} int x = intFunction(5);";
         string input5 = "intFunction : (int x, int z) -> int {z + x;} int x = 1; intFunction(x, x + 4);";
+        string input6 = "intFunction : (int x) -> int {int y = x + 10 * 5; x - 5;} intFunction(5.2);";
+
 
         string output1 = "fn intFunction(x: &i32) -> i32 {let y = *x + 10 * 5;*x - 5}intFunction(&(5));";
         string output2 = "let a = 10;fn intFunction(x: &i32) -> i32 {let a = 10;let y = *x + a * 5;*x - 5}intFunction(&(5));";
         string output3 = "fn intFunction(x: &i32) -> i32 {fn intFunctionNested(z: &i32) -> i32 {*z + 1}intFunctionNested(&(*x))}intFunction(&(5));";
         string output4 = "fn intFunction(x: &i32) -> i32 {let y = *x + 10 * 5;y}let x = intFunction(&(5));";
         string output5 = "fn intFunction(x: &i32, z: &i32) -> i32 {*z + *x}let x = 1;intFunction(&(x), &(x + 4));";
+        string output6 = "fn intFunction(x: &i32) -> i32 {let y = *x + 10 * 5;*x - 5}intFunction(&(5.2));";
+
 
         // Also tests conversion to call by reference
         AssertTrue(input1, output1); // Regular function call
@@ -121,9 +147,38 @@ public class TranspilerTests
         AssertTrue(input3, output3); // Function call with nested function
         AssertTrue(input4, output4); // Function call with assignment
         AssertTrue(input5, output5); // Function call with multiple arguments and expression to pointer
+        AssertIntegrationTrue(input5, output5);
+        AssertTrue(input6, output6);
+        AssertIntegrationFalse(input6);
     }
 
     private void AssertTrue(string input, string expectedOutput)
+    {
+        GtlLexer lexer = new GtlLexer(CharStreams.fromString(input));
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        GtlParser parser = new GtlParser(tokenStream)!;
+
+        parser.ErrorHandler = new ErrorStrategy();
+        parser.RemoveErrorListeners();
+        parser.AddErrorListener(new ErrorListener());
+
+        var parseTree = parser.program();
+        TestVisitor transvisitor = new TestVisitor();
+        _ = transvisitor.Visit(parseTree);
+        string workingDirectory = Environment.CurrentDirectory;
+        string gtlPath = Directory.GetParent(workingDirectory)!.Parent!.Parent!.Parent!.FullName;
+        string path = gtlPath + "/GameTheoryLanguage/output/src/main.rs";
+        string output = File.ReadAllText(path);
+        output = output.Replace("\r", "");
+        output = output.Replace("\n", "");
+        output = output.Remove(0, 143);
+        output = output.Remove(output.Length - 1, 1);
+        Console.WriteLine("Expected: " + expectedOutput);
+        Console.WriteLine("Received: " + output);
+        Assert.IsTrue(output.Equals(expectedOutput));
+    }
+
+    private void AssertIntegrationTrue(string input, string expectedOutput)
     {
         GtlLexer lexer = new GtlLexer(CharStreams.fromString(input));
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
@@ -150,7 +205,37 @@ public class TranspilerTests
         Console.WriteLine("Received: " + output);
         Assert.IsTrue(output.Equals(expectedOutput));
     }
+    private void AssertIntegrationFalse(string input)
+    {
+        GtlLexer lexer = new GtlLexer(CharStreams.fromString(input));
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        GtlParser parser = new GtlParser(tokenStream)!;
+
+        parser.ErrorHandler = new ErrorStrategy();
+        parser.RemoveErrorListeners();
+        parser.AddErrorListener(new ErrorListener());
+
+        var parseTree = parser.program();
+        CustomGtlVisitor visitor = new CustomGtlVisitor();
+        TestVisitor transvisitor = new TestVisitor();
+        try
+        {
+            // If both visitors succeeded without throwing an exception,
+            // it indicates an integration fault.
+            _ = visitor.Visit(parseTree);
+            _ = transvisitor.Visit(parseTree);
+        }
+        catch (Exception ex)
+        {
+            // If any exception is caught, it indicates a failure, which we expect for this test
+            Console.WriteLine("Exception caught: " + ex.Message);
+            return;
+        }
+        Assert.Fail("Integration fault: Expected an exception during visitation.");
+    }
 }
+
+
 class TestVisitor : TransVisitor
 {
     public override object VisitProgram([NotNull] GtlParser.ProgramContext context)
